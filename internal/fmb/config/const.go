b4180d3c54bdf742cc48258f4927440f3226e63b
Haski007 <haski007001@gmail.com>
2021-04-01 19:02:14 +0300

    refactor: changed help guide pic

--- internal/fmb/config/const.go
+++ internal/fmb/config/const.go
@@ -3,7 +3,7 @@ package config
 const (
 	ConfigFile                 = ".env.json"
 	GoogleAPIFile              = ".youtube_client_secret.json"
-	GuideGetPlaylistIDImageURL = "https://gitgub.com/"
+	GuideGetPlaylistIDImageURL = "https://github.com/Haski007/fav-music-bot/blob/master/resources/guide.png?raw=true"
 
 	LikedPlaylistID = "PLRJ34yHgNS9P3Lze8yZ1Z5uF_qRh8YLf1"
 )
--- internal/fmb/resource/cmd-help-handler.go
+++ internal/fmb/resource/cmd-help-handler.go
@@ -1,6 +1,7 @@
 package resource
 
 import (
+	"github.com/Haski007/fav-music-bot/internal/fmb/config"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
@@ -16,6 +17,10 @@ func (bot *FMBService) commandHelpHandler(update tgbotapi.Update) {
 		"2) Copy playlist id an showed on image below\n" +
 		"3) Use command in chat with bot\n" +
 		"/reg_playlist [paste id here]"
-	resp := tgbotapi.NewPhotoUpload(update.Message.Chat.ID,
-		"")
+
+	resp := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, nil)
+	resp.FileID = config.GuideGetPlaylistIDImageURL
+	resp.UseExisting = true
+	resp.Caption = message
+	bot.Bot.Send(resp)
 }
